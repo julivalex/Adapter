91c1a42775167af727b9e44b910442b2e7d2c8ca
Yulia <123>
2019-09-25 18:47:37 +0400

    init project

---
+++ src/com/company/adaptee/Turkey.java
@@ -0,0 +1,6 @@
+package com.company.adaptee;
+
+public interface Turkey {
+    void gobble();
+    void fly();
+}
---
+++ src/com/company/client/Duck.java
@@ -0,0 +1,6 @@
+package com.company.client;
+
+public interface Duck {
+    void quack();
+    void fly();
+}
